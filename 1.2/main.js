a8d15432c89d6cbf023aefff67bfc74c4578fe29
01joanna <joa.cuevas9@gmail.com>
2024-06-26 16:23:51 +0200

    ex 2

--- 1.2/main.js
+++ 1.2/main.js
@@ -26,9 +26,27 @@ const resultado = i > 0 ? "Positiu" : i < 0 ? "Negatiu" : "Zero"
 
 // console.log(resultado);
 
-// Operador ternari amb funcions: Crea una funció trobarMaxim que accepti tres paràmetres (a, b, c) i utilitzi l'operador ternari per determinar el valor màxim.k
+// Operador ternari amb funcions: Crea una funció trobarMaxim que accepti tres paràmetres (a, b, c) i utilitzi l'operador ternari per determinar el valor màxim.
 
+function trobarMaxim(a, b, c) {
+    return a > b ? a > c ? 
+    a : c : b > c ? b : c
+}
 
+// si a es mes gran que b, o a es mes gran que c, retorna a, sino b, si b es més gran que c retorna b, sino c.
+
+const res = trobarMaxim(4, 5, 9);
+// console.log(res)
 
 // ex 4
 // Operador ternari dins un bucle: Escriu una funció parOImpar que accepti un array de números i utilitzi un bucle per a recórrer l'array. Dins del bucle, utilitza l'operador ternari per a determinar si cada número és parell o imparell.
+
+function parOImpar(nums) {
+    nums.forEach(e => {
+        e % 2 === 0 ? 
+        console.log(`${e} es parell`) : console.log(`${e} es imparell`) 
+    });
+}
+
+const nums = [55, 6, 5]
+// parOImpar(nums)
